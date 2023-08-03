38a70ce3f302753522ffe54687614f922e30172e
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-08-04 00:09:42 +0600

    design improved

--- url-shortener-client/src/components/RouteRestriction/PrivateRoute.js
+++ url-shortener-client/src/components/RouteRestriction/PrivateRoute.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
+import "./PrivateRoute.css"
+
 const PrivateRoute = ({ isLoggedIn, isLoading }) => {
 
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className='loading'><img src="https://i.gifer.com/YCZH.gif" alt="" /></div>;
   }
 
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
